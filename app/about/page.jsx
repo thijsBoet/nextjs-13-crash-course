7c59ebdd4b5dad8bab8b20bd48168b0ef4de9d9b
thijsBoet <m.boet@chello.nl>
2023-04-11 14:03:44 +0200

    init commit

---
+++ app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            <h1>About WebdeF</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam corporis, ea harum quae veritatis laboriosam non eligendi iure exercitationem quasi deserunt impedit nobis ducimus sunt ipsam modi. Dolore quidem labore aut, laboriosam suscipit natus totam obcaecati dicta vitae, hic provident quos quasi voluptatem consectetur fugiat. Quo nihil adipisci modi repellat.</p>
+        </div>
+    )
+}
+
+export default page
---
+++ app/about/team/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            <h1>Our Team</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque voluptatibus ipsa aspernatur sit voluptas in exercitationem explicabo deleniti? Esse officiis quisquam maxime molestiae dicta. Adipisci.</p>
+        </div>
+    )
+}
+
+export default page
